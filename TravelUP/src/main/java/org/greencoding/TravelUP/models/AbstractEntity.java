57a39ad3e91b9e66d3e8494fe76269caed1a8f07
anamindful <anamindful@gmail.com>
2023-12-18 21:21:26 -0600

    bringing my branch up to date

--- TravelUP/src/main/java/org/greencoding/TravelUP/models/AbstractEntity.java
+++ TravelUP/src/main/java/org/greencoding/TravelUP/models/AbstractEntity.java
@@ -4,11 +4,25 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 
+import jakarta.persistence.*;
+
 @MappedSuperclass
 public abstract class AbstractEntity {
 
     @Id
-    @GeneratedValue
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public AbstractEntity() {
+        // Default constructor
+    }
+
+    public AbstractEntity(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 
 }
